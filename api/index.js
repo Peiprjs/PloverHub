be6f80eab12bed1fb081cdbfa2ec168b6eea4f46
Peipr <peipryt@gmail.com>
2021-01-29 07:23:49 +0000

    Fixed the website

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express.app
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get('/', function(req, res) {
-  res.send("You\'re not supposed to be here silly!'")
-})
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/file1.html'));
+});
+
+app.use('/', router);
+app.listen(process.env.port || 2000);
+
+console.log('Running at Port 2000');
