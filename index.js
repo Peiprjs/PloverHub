721af57fa2f8ed16a8d3bc8a86019fbcb8da1efc
Peiprjs <peipryt@gmail.com>
2021-02-04 07:41:30 +0000

    Parsing, beautifying, stuck on SQL

--- index.js
+++ index.js
@@ -10,29 +10,30 @@ const prefix = (",");
 const fs = require('fs');
 //////////////////////////////////////////////////////
 const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: 'database.sqlite',
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    // SQLite only
+    storage: 'database.sqlite',
 });
 const Tags = sequelize.define('tags', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	description: Sequelize.TEXT,
-	username: Sequelize.STRING,
-	usage_count: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0,
-		allowNull: false,
-	},
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+    },
+    description: Sequelize.TEXT,
+    username: Sequelize.STRING,
+    usage_count: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+    },
 });
 
 //////////////////////////////////////////////////////
 client.once('ready', () => {
-  console.log(`PloverHub is ready to roll!`);
+    console.log(`PloverHub is ready to roll!`);
+    Tags.sync();
 });
 client.login(`${config}`);
 console.log('Logged in, connection appears good');
@@ -40,15 +41,34 @@ console.log('Logged in, connection appears good');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-  const command = require(`./cmds/${file}`);
-  client.commands.set(command.name, command);
+    const command = require(`./cmds/${file}`);
+    client.commands.set(command.name, command);
 }
 ///////////////////////////////////////////////////////
 client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-//////////////////////////////////////////////////////
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    //////////////////////////////////////////////////////
+    if (command === "register") {
 
+
+        try {
+            // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+            const tag = await Tags.create({
+                name: tagName,
+                description: tagDescription,
+                username: message.author.username,
+            });
+            return message.reply(`Tag ${tag.name} added.`);
+        }
+        catch (e) {
+            if (e.name === 'SequelizeUniqueConstraintError') {
+                return message.reply('That tag already exists.');
+            }
+            return message.reply('Something went wrong with adding a tag.');
+        }
+
+    }
 });
